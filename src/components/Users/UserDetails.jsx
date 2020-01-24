bbce1c5ab5f233256a3045b8dc2f9483a4bb904d
fiqriismail <fiqri@peachit.lk>
2020-01-24 14:04:11 +0530

    Added user details

--- src/components/Users/UserDetails.jsx
+++ src/components/Users/UserDetails.jsx
@@ -3,7 +3,29 @@ import React from 'react';
 import HeaderTitle from './../shared/HeaderTitle';
 
 function UserDetails() {
-  return <HeaderTitle title="User Details" tagline="Details of user 1" />;
+  return (
+    <React.Fragment>
+      <HeaderTitle title="User Details" tagline="Details of user 1" />
+
+      <div className="card" style={{ width: 256 }}>
+        <img
+          src="https://randomuser.me/api/portraits/med/men/76.jpg"
+          className="card-img-top"
+          width="256"
+          height="256"
+          alt=""
+        />
+        <div className="card-body">
+          <h5 className="card-title">Mr. Brad Gibson (26)</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <p>brad.gibson@example.com</p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default UserDetails;
--- src/components/Users/UserListItem.jsx
+++ src/components/Users/UserListItem.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
-function UserListItem() {
-  return <p>Items</p>;
+function UserListItem(props) {
+  return (
+    <tr>
+      <td className="text-center">
+        <img
+          src={props.profileUrl}
+          width="64"
+          height="64"
+          alt={props.fullname}
+          className="border rounded-circle shadow-sm"
+        />
+      </td>
+      <td>{props.fullname}</td>
+      <td>{props.email}</td>
+      <td>{props.age}</td>
+    </tr>
+  );
 }
 
 export default UserListItem;
