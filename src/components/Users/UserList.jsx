ae378f904ad91d21f606edab3556a6f25c7efcfc
fiqriismail <fiqri@peachit.lk>
2020-01-24 12:36:43 +0530

    Formatting user list page

--- src/components/Users/UserList.jsx
+++ src/components/Users/UserList.jsx
@@ -3,7 +3,53 @@ import React from 'react';
 import HeaderTitle from './../shared/HeaderTitle';
 
 function UserList() {
-  return <HeaderTitle title="User List" tagline="All the users we can find" />;
+  return (
+    <React.Fragment>
+      <HeaderTitle title="User List" tagline="All the users we can find" />
+      <div className="container">
+        <table className="table table-bordered">
+          <thead className="thead-light">
+            <tr>
+              <th>Avatar</th>
+              <th>Full Name</th>
+              <th>Email</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="text-center">
+                <img
+                  src="https://randomuser.me/api/portraits/med/men/75.jpg"
+                  width="64"
+                  height="64"
+                  alt="Avatar"
+                  className="border rounded-circle shadow-sm"
+                />
+              </td>
+              <td>Mr. Brad Gibson</td>
+              <td>brad.gibson@example.com</td>
+              <td>26</td>
+            </tr>
+            <tr>
+              <td className="text-center">
+                <img
+                  src="https://randomuser.me/api/portraits/med/men/75.jpg"
+                  width="64"
+                  height="64"
+                  alt="Avatar"
+                  className="border rounded-circle shadow-sm"
+                />
+              </td>
+              <td>Mr. Brad Gibson</td>
+              <td>brad.gibson@example.com</td>
+              <td>26</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default UserList;
---
+++ src/components/Users/UserListItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function UserListItem() {
+  return <p>Items</p>;
+}
+
+export default UserListItem;
